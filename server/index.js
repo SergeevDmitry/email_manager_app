b8069a134dedd23c872c388430baf6a17d1fc651
sergeevdms <sergeevdms@gmail.com>
2018-10-25 22:51:10 +0300

    added files

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 
